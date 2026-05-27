c03237be15b95606bdebcf7f12052c4ada3bec62
Mordy Pesach <mordypassover@gmail.com>
2026-05-27 14:47:01 +0300

    fix create shape

--- shape_manager.py
+++ shape_manager.py
@@ -1,21 +1,33 @@
 import json
+import logging
 from square import Square
 from circle import Circle
 from rectangle import Rectangle
 
 
+logging.basicConfig(level=logging.INFO, format='%(levelname)s | %(message)s  | %(asctime)s', encoding="utf-8")
+logger = logging.getLogger(__name__)
+
+
 class ShapeManager:
     def __init__(self):
         self.shapes = []
-       # self.load_from_json()
+        # self.load_from_json()
 
     def create_shape(self, shape):
+        logging.info("started to create a shape")
         shape_dict = {"square" : Square, "circle" : Circle, "rectangle" : Rectangle}
         if shape in shape_dict:
-            self.shapes.append(shape_dict[shape]().to_dict())
-
+            try:
+                self.shapes.append( shape_dict[shape]())
+            except ValueError :
+                logging.error("input not valid!")
+            logging.info("finished to create shape")
+        else:
+            logging.error(f"shape -{shape} not supported")
     def get_all_shapes(self):
-        pass
+        return self.shapes
+
     def update_shape(self, shape_id, new_data):
         pass
     def delete_shape(self, shape_id):
@@ -25,3 +37,8 @@ class ShapeManager:
     def load_from_json(self):
         with open("shapes.json", "r", encoding="utf-8") as f:
             return json.load(f)
+
+if __name__ == "__main__":
+    sm = ShapeManager()
+    sm.create_shape("square")
+    print(sm.shapes )
