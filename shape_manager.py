a2d38a58bcbfd3a11ca2892b0a7832ff3dc5b18e
Mordy Pesach <mordypassover@gmail.com>
2026-05-27 16:25:55 +0300

    add update shape fonc

--- shape_manager.py
+++ shape_manager.py
@@ -3,6 +3,7 @@ import logging
 from square import Square
 from circle import Circle
 from rectangle import Rectangle
+from shape import Shape
 
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s | %(message)s  | %(asctime)s', encoding="utf-8")
@@ -14,7 +15,7 @@ class ShapeManager:
         self.shapes = []
         # self.load_from_json()
 
-    def create_shape(self, shape:str,  param_s:tuple):
+    def create_shape(self, shape:str,  param_s:tuple ,shape_id = Shape.counter):
         logging.info("started to create a shape")
         shape_dict = {"square" : Square, "circle" : Circle, "rectangle" : Rectangle}
         if shape in shape_dict:
@@ -29,7 +30,12 @@ class ShapeManager:
         return self.shapes
 
     def update_shape(self, shape_id, new_data):
-        pass
+        for instance in self.get_all_shapes():
+            if instance.id == shape_id :
+                self.delete_shape(shape_id)
+                self.create_shape(instance.shape_type, new_data,shape_id = shape_id)
+
+
     def delete_shape(self, shape_id):
         pass
     def save_to_json(self):
