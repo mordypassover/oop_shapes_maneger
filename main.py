7b2c5380089b525a741267390d03d3a5ffc4ce31
Mordy Pesach <mordypassover@gmail.com>
2026-05-27 14:45:55 +0300

    added main build

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 import shape_manager
 
 
-def add_shape():
-    pass
+def add_shape(shape):
+    SHAPEMANAGER.create_shape(shape)
+
+
 def show_all_shapes():
     pass
+
+
 def update_shape():
     pass
+
+
 def delete_shape():
     pass
+
+
+def menu():
+    print("to add shape enter 1\n"
+          "to show all shapes enter 2\n"
+          "to update shape enter 3\n"
+          "to delete sape enter 4\n"
+          "to exit enter 5")
+
+
 def main():
-    pass
+    SHAPEMANAGER = shape_manager.ShapeManager()
+    flag = True
+    while flag:
+        menu()
+        if 1:
+            add_shape()
+        elif 2:
+            show_all_shapes()
+        elif 3:
+            update_shape()
+        elif 4:
+            delete_shape()
+
+        elif 5:
+            flag = False
+        else:
+            pass
