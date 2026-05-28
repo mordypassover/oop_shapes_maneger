55c74fb5f3d21a738ca34655a1c1184ac2b0d626
Mordy Pesach <mordypassover@gmail.com>
2026-05-28 11:55:43 +0300

    fix method name

--- shape_manager.py
+++ shape_manager.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
     sm.create_shape("square", (3,))
     sm.create_shape("rectangle",(4, 5))
     print(sm.get_all_shapes() )
-    print(sm.get_all_shapes()[1].id)
+    print(sm.get_all_shapes()[1].shape_id)
     print(sm.get_all_shapes()[2].shape_type)
 
     sm.save_to_json()
