3c1659a49aa55fea2ca3bc8bc872f79edce23435
Mordy Pesach <mordypassover@gmail.com>
2026-06-02 10:13:04 +0300

    add get all shaps rout

--- server.py
+++ server.py
@@ -5,3 +5,9 @@ import uvicorn
 
 app = FastAPI()
 SHAPE_MANAGER = shape_manager.ShapeManager()
+
+
+@app.get("/shapes")
+def get_all_shapes_as_dicts():
+    return SHAPE_MANAGER.show_shapes_as_dicts()
+
