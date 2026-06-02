378a6793b9e41ec35560a489fffa8a79127effb8
Mordy Pesach <mordypassover@gmail.com>
2026-06-02 10:52:28 +0300

    add get singe

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
+from requests import Response
+
 import shape_manager
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import uvicorn
 
 
@@ -11,3 +13,12 @@ SHAPE_MANAGER = shape_manager.ShapeManager()
 def get_all_shapes_as_dicts():
     return SHAPE_MANAGER.show_shapes_as_dicts()
 
+
+@app.get("/shapes/{id}", status_code=200)
+def get_1_shape_by_id(id : int,  response: Response):
+    all_shapes = SHAPE_MANAGER.show_shapes_as_dicts()
+    for shape in all_shapes:
+        if int(id) == shape["id"]:
+            return shape
+    response.status_code = 404
+    return {"status" : "shape id not found"}
